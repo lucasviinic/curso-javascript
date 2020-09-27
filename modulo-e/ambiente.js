45d51e4b9e434c295c715bffa67972a3d9d39508
Lucas Vinícius <lucas.vmsilva@ufrpe.br>
2020-09-27 10:12:52 -0300

    Estruturas "while" e "do while"

---
+++ modulo-e/ambiente.js
@@ -0,0 +1,16 @@
+var a = 1
+var b = 1
+
+//while
+console.log('Estrutura "while"')
+while (a <= 5){
+    console.log(`Passo ${a}`)
+    a++
+}
+//do while
+console.log('Estrutura "do while"')
+do {
+    console.log(`Passo ${b}`)
+    b++
+} while (b <= 5)
+

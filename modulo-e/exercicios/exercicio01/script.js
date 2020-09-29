0eed7b860689b557da4d7ce010ba392b368622b6
Lucas Vinícius <lucas.vmsilva@ufrpe.br>
2020-09-29 09:37:25 -0300

    Adicionado esceção para o passo não ser igual a zero

--- modulo-e/exercicios/exercicio01/script.js
+++ modulo-e/exercicios/exercicio01/script.js
@@ -1,17 +1,25 @@
 function contar(){
-    var i = Number(document.getElementById('txti').value)
-    var f = Number(document.getElementById('txtf').value)
-    var p = Number(document.getElementById('txtp').value)
+    var i = document.getElementById('txti').value
+    var f = document.getElementById('txtf').value
+    var p = document.getElementById('txtp').value
     var res = document.getElementById('res')
     var result = ``
     
-    if (i.length == 0 || f.length == 0 || p.length == 0){
+    if (i.length == 0 || f.length == 0){
         window.alert('[ERRO] Faltam dados!')
-    } else {
-        for (i; i <= f; i+=p){
-            result += `${i} <span>&#128073;</span>`
-        }
-        res.innerHTML = `<p>Contando:</p>`
-        res.innerHTML += `<p>${result} <span>&#127937;</span></p>` 
+    } else if (Number(p) == 0){
+        p = 1
+        window.alert(`Passo invalido, considerando PASSO = ${p}`)
     }
+    
+    i = Number(i)
+    f = Number(f)
+    p = Number(p)
+    for (i; i <= f; i+=p){
+        result += `${i} <span>&#128073;</span>`
+    }
+
+    res.innerHTML = `<p>Contando:</p>`
+    res.innerHTML += `<p>${result} <span>&#127937;</span></p>` 
+
 }
